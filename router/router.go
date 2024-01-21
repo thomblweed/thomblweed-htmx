7bace9670de3b18320c4d2a8a40e5fccf3a72cc7
thom <thomblweed@gmail.com>
2024-01-21 16:22:04 +0000

    fonts setup with stylesheets poc

--- router/router.go
+++ router/router.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
-	"github.com/thomblweed/thomblweed-htmx/views"
+	"github.com/thomblweed/thomblweed-htmx/layouts"
 )
 
 func Home() chi.Router {
 	router := chi.NewRouter()
 	router.Handle("/static/*", http.StripPrefix("/static", http.FileServer(http.Dir("./public"))))
-	router.Get("/", templ.Handler(views.Home()).ServeHTTP)
+	router.Get("/", templ.Handler(layouts.MainLayout()).ServeHTTP)
 
 	return router
 }
